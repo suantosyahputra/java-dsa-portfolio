743c0314c0e803b981724d137eee6c2c94284ddf
Suanto Syahputra <suanto.syahputra@gmail.com>
2026-03-10 01:35:44 +0100

    feat: implement bubble sort

---
+++ src/main/java/dsa/algorithms/sorting/BubbleSort.java
@@ -0,0 +1,45 @@
+package dsa.algorithms.sorting;
+
+public class BubbleSort {
+
+    public static void sort(int[] arr) {
+
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            for (int j = 0; j < n - i - 1; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                }
+            }
+        }
+    }
+
+    public static void printArray(int[] arr) {
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 3, 8, 4, 2};
+
+        System.out.print("before: ");
+        printArray(arr);
+
+        sort(arr);
+
+        System.out.print("after: ");
+        printArray(arr);
+    }
+}
